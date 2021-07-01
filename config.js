b372c3d95f86eca8567a9fd7d1edf8d2405538fe
Tannieyup <86703097+Tannieyup@users.noreply.github.com>
2021-07-01 10:28:07 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,17 @@
 const CONFIG = {
-    introTitle: 'Huy ơi!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao anh cứ đi nhầm đường
-    Lạc vào tim em lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'dzui hong mà cười dữ vậy =)))',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Yêu bé Tiên vô cùnggg',
-    btnNo: 'Còn lâu mới yêu :v',
+    introTitle: 'Huy ơi!!!',
+    introDesc: `Chú bé loắt choắt, cái sắc xinh xinh
+    Cái chân thoăn thoắt, cái đầu nhớ anh :v`,
+    btnIntro: 'Một nhấp vào em đi anh =)))',
+    title: 'Phải chăng anh đã yêu ngay từ cái nhìn đầu tiên',
+    desc: 'Phải chăng anh đã say ngay từ lúc thấy nụ cười ấy ',
+    btnYes: 'Vẫn là yêu em nhưng nhiều hơn cái kia một chút',
+    btnNo: 'Yêu em',
     question:'Trên thế giới hơn 7 tỉ người mà sao anh lại yêu em dạ???',
-    btnReply: 'Tại vì bé Tiên hết sức là đáng yêu ròiiii',
-    reply: 'Yêu thì yêu mà không yêu thì yêu',
+    btnReply: 'Nhấp em nữa đi =)))',
+    reply: 'Tại vì bé Tiên hết sức là đáng yêu ròiiii',
     mess: 'Em biết mà. Yêu anh nhiều nhiều',
     messDesc: 'Tối nay cho em xem chuối nữa nha =)))',
-    btnAccept: 'oke luônnnn',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    btnAccept: 'oke luônnnn :v gửi cho em 1 chục cái clip',
+    messLink: 'https://www.facebook.com/thuytien23061991' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
